d745464ac22eefe927241d04bf9f076c166d1877
John Florian <jflorian@doubledog.org>
2010-12-06 18:59:01 -0500

    New - /etc/network symlink

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,15 @@ class network {
         default         => "eth0",
     }
 
+    # Set up a link of /etc/network to /etc/sysconfig/network-scripts/ because
+    # the latter is such a stupid PITA.
+    file { "/etc/network":
+        ensure  => "sysconfig/network-scripts/",
+    }
+
     # Configure the DHCP client to not manage NTP settings.  We want DHCP to
     # configure NTP right up until we begin having puppet manage a host.
-    line {"disable PEERNTP":
+    line { "disable PEERNTP":
         ensure  => present,
         file    => "/etc/sysconfig/network-scripts/ifcfg-${interface}",
         line    => "PEERNTP=no",
