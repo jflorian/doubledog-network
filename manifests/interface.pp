e47b4e3c663ede09dfdcba3d5e4b6a6ccec102e2
John Florian <jflorian@doubledog.org>
2014-01-11 14:22:43 -0500

    New - network module improvements
    - Rdoc format
    - fix missing parameter docs
    - more versatile interface templates for static setups
    - STP and gateway are now parameterized

--- manifests/interface.pp
+++ manifests/interface.pp
@@ -1,17 +1,48 @@
 # modules/network/manifests/interface.pp
 #
-# Synopsis:
-#       Installs a network interface configuration file.
+# == Define: network::interface
 #
-# Parameters:
-#       Name__________  Default_______  Description___________________________
+# Installs an interface configuration file for the network services.
 #
-#       name                            interface name (e.g., 'eth0')
-#       ensure          present         configuration is to be present/absent
-#       template        standard        particular template to be used
+# === Parameters
 #
-# Requires:
-#       Class['network']
+# [*namevar*]
+#   Name of the interface, e.g., 'eth0'.  Required.
+#
+# [*ensure*]
+#   Instance is to be 'present' (default) or 'absent'.
+#
+# [*template*]
+#   The particular template to be used.  Must be one of 'dhcp', 'dhcp-bridge',
+#   'static' or 'static-bridge'.
+#
+# [*ip_address*]
+#   The address to be assigned to the interface.  Required for the static
+#   templates and ignored for the dhcp templates.
+#
+# [*gateway*]
+#   The default route address to be assigned to the interface.  Recommended
+#   for the static templates and ignored for the dhcp templates.
+#
+# [*bridge*]
+#   Name of the associated bridge interface, if any.  Optional.  Ignored for
+#   the bridge templates.
+#
+# [*peer_dns*]
+#   Use the name servers provided by DHCP?  Must be one of 'yes' (default) or
+#   'no'.  Ignored for the static templates.
+#
+# [*peer_ntp*]
+#   Use the time servers provided by DHCP?  Must be one of 'yes' (default) or
+#   'no'.  Ignored for the static templates.
+#
+# [*stp*]
+#   Enable the Spanning Tree Protocol (STP)?  Must be one of 'yes' (default)
+#   or 'no'.  Ignored for all but the bridge templates.
+#
+# === Authors
+#
+#   John Florian <jflorian@doubledog.org>
 
 
 define network::interface (
@@ -19,9 +50,11 @@ define network::interface (
         $template='standard',
         $ip_address=undef,
         $netmask=undef,
+        $gateway=undef,
         $bridge=undef,
         $peer_dns='yes',
         $peer_ntp='yes',
+        $stp='yes',
     ) {
 
     # The template needs a particular macaddress fact, but it cannot do
@@ -31,15 +64,15 @@ define network::interface (
     $interface_hwaddr = inline_template("<%= scope.lookupvar(@mac_fact) %>")
 
     file { "/etc/sysconfig/network-scripts/ifcfg-${name}":
-        content => template("network/ifcfg-${template}"),
         ensure  => $ensure,
+        owner   => 'root',
         group   => 'root',
         mode    => '0640',
-        notify  => Service['network'],
-        owner   => 'root',
         selrole => 'object_r',
         seltype => 'net_conf_t',
         seluser => 'system_u',
+        content => template("network/ifcfg-${template}"),
+        notify  => Service['network'],
     }
 
 }
